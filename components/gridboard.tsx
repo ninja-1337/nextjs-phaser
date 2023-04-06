bc046e9dee91866e9ded40f243bc21859104fb31
Your Name <you@example.com>
2023-04-06 22:27:43 +0300

    first working model for vim game

--- components/gridboard.tsx
+++ components/gridboard.tsx
@@ -1,18 +1,11 @@
 import { useRef } from "react";
 import { useGame } from "../helpers/useGame";
 import { Types } from "phaser";
-import { MainScene } from "./mainScene";
+import { MainScene } from "./../components/mainScene";
 
 const gameConfig: Types.Core.GameConfig = {
   width: "100%",
   height: "100%",
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 0 },
-      debug: false, // Set to true to enable physics debugging
-    },
-  },
   type: Phaser.AUTO,
   scale: {
     mode: Phaser.Scale.FIT,
@@ -26,16 +19,15 @@ const gameConfig: Types.Core.GameConfig = {
   scene: MainScene,
 };
 
-// you can place this in a different file if you prefer
-const PhaserGame = () => {
+const GridBoard = () => {
   const parentEl = useRef<HTMLDivElement>(null);
   useGame(gameConfig, parentEl);
 
   return (
-    <div className="container">
-      <div ref={parentEl} />
-    </div>
+    <>
+      <div className="container" ref={parentEl} />
+    </>
   );
 };
 
-export default PhaserGame;
+export default GridBoard;
--- components/mainScene.tsx
+++ components/mainScene.tsx
@@ -1,24 +1,89 @@
 import { Scene, GameObjects, Cameras } from "phaser";
-
 export class MainScene extends Scene {
-  private helloLabel!: GameObjects.Text;
+  private grid!: Phaser.GameObjects.Grid;
+  private helloLabel!: Phaser.GameObjects.Text;
+  private player!: Phaser.Physics.Arcade.Sprite;
   private camera!: Cameras.Scene2D.Camera;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
   init() {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor("#24252A");
   }
 
   create() {
-    const { centerX, centerY } = this.camera;
-    this.helloLabel = this.add
-      .text(centerX, centerY, "NextJs Phaser", {
-        fontSize: 40,
-      })
-      .setShadow(5, 5, "#5588EE", 0, true, true)
-      .setOrigin(0.5, 0.5);
-  }
+        // Enable keyboard input
+        this.input.keyboard.enabled = true;
 
-  update() {
-    this.helloLabel.angle += 0.1;
+        // Create cursor keys
+        this.cursors = this.input.keyboard.createCursorKeys();
+    
+    const { width, height } = this.sys.game.canvas;
+    this.grid = this.add.grid(
+      width / 2,
+      height / 2,
+      1000,
+      1000,
+      100,
+      100,
+      0xffffff,
+      0.2,
+      0 // specify the size of each cell
+    );
+   // Add a player sprite to the center of the grid
+   this.player = this.physics.add.sprite(
+    width / 2,
+    
+    height / 2,
+    "player"
+  );
+  this.player.setAccelerationX(0);
+  this.player.setAccelerationY(0);
+  this.player.setBounce(0.2);
+  this.player.setCollideWorldBounds(true);  
+  
+  
+  this.player.setActive(true);
+  this.player.setDepth(1); // ensure the player sprite appears on top of the grid
+
+   
+  }
+  update(){
+    if (this.cursors.left.isDown)
+{
+    this.player.setVelocityX(-460);
+
+    this.player.anims.play('left', true);
+}
+else if (this.cursors.right.isDown)
+{
+    this.player.setVelocityX(460);
+
+    this.player.anims.play('right', true);
+}
+else if (this.cursors.up.isDown)
+{
+    this.player.setVelocityY(-460);
+
+    this.player.anims.play('up', true);
+}
+else if (this.cursors.down.isDown)
+{
+    this.player.setVelocityY(460);
+
+    this.player.anims.play('up', true);
+}
+else
+{
+    this.player.setVelocityX(0);
+    this.player.setVelocityY(0);
+
+    this.player.anims.play('turn');
+}
+
+if (this.cursors.up.isDown && this.player.body.touching.down)
+{
+    this.player.setVelocityY(-330);
+}
   }
 }
